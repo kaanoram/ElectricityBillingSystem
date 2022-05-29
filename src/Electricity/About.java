4349a6fcd6b14d039369053c26767995cf52bbb3
Kaan Oram <kaanoram@gmail.com>
2022-05-28 20:16:01 -0500

    Updated About.java

--- src/Electricity/About.java
+++ src/Electricity/About.java
@@ -21,42 +21,41 @@ public class About extends JFrame implements ActionListener {
     public About() {
 
         setLayout(null);
-        JButton b1 = new JButton("Exit");
+        b1 = new JButton("Exit");
         add(b1);
         b1.setBounds(180, 430, 120, 20);
         b1.addActionListener(this);
 
-        Font f = new Font("RALEWAY", Font.BOLD, 180);
+        f = new Font("RALEWAY", Font.BOLD, 180);
         setFont(f);
 
-        s = "                                    About Projects          \n  "
-                + "\nElectricity Billing System is a software-based application "
-                + "developed in Java programming language. The project aims at serving"
-                + "the department of electricity by computerizing the billing system. "
-                + "It mainly focuses on the calculation of Units consumed during the "
-                + "specified time and the money to be paid to electricity offices. "
-                + "This computerized system will make the overall billing system easy, "
-                + "accessible, comfortable and effective for consumers.\n\n"
+        s = """
+            Electricity Billing System is a software-based application developed in Java programming language. 
+            The project aims at serving the department of electricity by computerizing the billing system. 
+            It mainly focuses on the calculation of Units consumed during the specified time and the money to be paid to electricity offices. 
+            This computerized system will make the overall billing system easy, accessible, comfortable and effective for consumers.
+            
+            """
                 ;
 
-        TextArea t1 = new TextArea(s, 10, 40, Scrollbar.VERTICAL);
+        t1 = new TextArea(s, 10, 40, Scrollbar.VERTICAL);
         t1.setEditable(false);
         t1.setBounds(20, 100, 450, 300);
 
         add(t1);
 
-        Font f1 = new Font("RALEWAY", Font.BOLD, 16);
+        f1 = new Font("RALEWAY", Font.BOLD, 16);
         t1.setFont(f1);
 
         Container contentPane = this.getContentPane();
         t1 = new TextArea();
 
-        JLabel l1 = new JLabel("About Project");
+        l1 = new JLabel("About Project");
         add(l1);
         l1.setBounds(170, 10, 180, 80);
         l1.setForeground(Color.red);
 
-        Font f2 = new Font("RALEWAY", Font.BOLD, 20);
+        f2 = new Font("RALEWAY", Font.BOLD, 20);
         l1.setFont(f2);
 
         setBounds(700, 220, 500, 550);
@@ -66,6 +65,7 @@ public class About extends JFrame implements ActionListener {
         
     }
 
+    @Override
     public void actionPerformed(ActionEvent e) {
         dispose();
     }
